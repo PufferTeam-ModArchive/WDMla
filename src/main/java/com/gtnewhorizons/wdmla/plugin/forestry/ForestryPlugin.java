9cf4da9c0d4655a566e88cc4d714ef6808c4c967
Quarri6343 <kaito6343@gmail.com>
2025-05-07 12:10:28 +0900

    supported forestry scoop icon

---
+++ src/main/java/com/gtnewhorizons/wdmla/plugin/forestry/ForestryPlugin.java
@@ -0,0 +1,27 @@
+package com.gtnewhorizons.wdmla.plugin.forestry;
+
+import com.gtnewhorizons.wdmla.api.IWDMlaClientRegistration;
+import com.gtnewhorizons.wdmla.api.IWDMlaPlugin;
+import com.gtnewhorizons.wdmla.api.Identifiers;
+import com.gtnewhorizons.wdmla.api.WDMlaPlugin;
+import com.gtnewhorizons.wdmla.config.WDMlaConfig;
+import forestry.apiculture.blocks.BlockBeehives;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.config.Configuration;
+
+@WDMlaPlugin(uid = "forestry", dependencies = "Forestry")
+public class ForestryPlugin implements IWDMlaPlugin {
+
+    @Override
+    public void registerClient(IWDMlaClientRegistration registration) {
+        registration.registerHarvest(ForestryToolHarvestHandler.INSTANCE, BlockBeehives.class);
+
+        WDMlaConfig.instance()
+                .getCategory(Identifiers.CONFIG_AUTOGEN + Configuration.CATEGORY_SPLITTER + "forestry")
+                .setLanguageKey("provider.wdmla.forestry.category");
+    }
+
+    public static ResourceLocation path(String path) {
+        return new ResourceLocation("forestry", path);
+    }
+}
---
+++ src/main/java/com/gtnewhorizons/wdmla/plugin/forestry/ForestryToolHarvestHandler.java
@@ -0,0 +1,42 @@
+package com.gtnewhorizons.wdmla.plugin.forestry;
+
+import com.gtnewhorizons.wdmla.api.TooltipPosition;
+import com.gtnewhorizons.wdmla.api.harvestability.EffectiveTool;
+import com.gtnewhorizons.wdmla.api.harvestability.HarvestabilityInfo;
+import com.gtnewhorizons.wdmla.api.harvestability.HarvestabilityTestPhase;
+import com.gtnewhorizons.wdmla.api.provider.HarvestHandler;
+import forestry.plugins.PluginApiculture;
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.ResourceLocation;
+
+import java.util.Arrays;
+
+//TODO: grafter, wrench
+public enum ForestryToolHarvestHandler implements HarvestHandler {
+    INSTANCE;
+
+    private static final EffectiveTool SCOOP = new EffectiveTool("scoop", Arrays.asList(PluginApiculture.items.scoop.getItemStack()));
+
+    @Override
+    public boolean testHarvest(HarvestabilityInfo info, HarvestabilityTestPhase phase,
+                               EntityPlayer player, Block block, int meta, MovingObjectPosition position) {
+        if (phase == HarvestabilityTestPhase.EFFECTIVE_TOOL_NAME) {
+            if (info.getEffectiveTool().isSameTool(SCOOP)) {
+                info.setEffectiveTool(SCOOP);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public ResourceLocation getUid() {
+        return ForestryPlugin.path("tool_harvest");
+    }
+
+    @Override
+    public int getDefaultPriority() {
+        return TooltipPosition.TAIL;
+    }
+}
